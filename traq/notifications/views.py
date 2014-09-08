f3fa0326fb0726a4076e875729b47384f92218e2
Bret Comnes <bcomnes@gmail.com>
2014-09-08 12:48:00 -0700

    Refactored notifications route
    Implemented a try catch statement and some 
    exception handling.

--- traq/notifications/views.py
+++ traq/notifications/views.py
@@ -5,12 +5,24 @@ from django.views.decorators.csrf import csrf_exempt
 
 from traq.local_settings import MAILGUN_API_KEY
 
+class DoesNotVerify(Exception):
+    def __str__(self):
+        return "Request failed to validate"
+
+class InvalidSlug(Exception):
+    def __str__(self):
+        return "Recipient field failed to validate"
+
 def verify(api_key, token, timestamp, signature):
     # from securing webhooks http://documentation.mailgun.com/user_manual.html#webhooks
-    return signature == hmac.new(
-                             key=api_key,
-                             msg= timestamp + token,
-                             digestmod=hashlib.sha256).hexdigest()
+    req_hash = hmac.new(
+                        key=api_key,
+                        msg= timestamp + token,
+                        digestmod=hashlib.sha256).hexdigest()
+    if signature == hmac.new
+        return
+    else
+        raise DoesNotVerify()
 
 def get_slug(address):
     # this should do some simple validation and return the target project slug
@@ -25,28 +37,33 @@ def index(request):
         return HttpResponse("Hi!  You don't look like email")
 
     elif request.method == 'POST':
+        response = HttpResponse()
 
-        api_key = MAILGUN_API_KEY
-        token = request.POST['token']
-        timestamp = request.POST['timestamp']
-        signature = request.POST['signature']
-
-        validate_request = verify(api_key, token, timestamp, signature)
-
-        if validate_request:
-
+        try:
+            token = request.POST['token']
+            timestamp = request.POST['timestamp']
+            signature = request.POST['signature']
             recipient = request.POST['recipient']
             subject = request.POST['subject']
-            body = request.POST['']
-            slug = get_slug(recipient);
-            project = Projects.objects.get(slug = slug)
-            t = Ticket(project = project, name = subject, )
-            print recipient
-            return HttpResponse("POST Request received :)")
+            body = request.POST['body?']
+            api_key = MAILGUN_API_KEY
+            slug = get_slug(recipient)
 
-        else:
-            response = HttpResponse("Your request didnt validate")
+            verify(api_key, token, timestamp, signature)
+            project = Projects.objects.get(slug = slug)
+            t = Ticket(project = project, name = subject, body = body)
+            t.save()
+            response.content = "A new ticket has been created"
+        except DoesNotExist:
+            response.content = "That project does not exist"
             response.status_code = 406
+        except DoesNotVerify:
+            response.content = "Invalid mailgun token"
+            response.status_code = 406
+        except InvalidSlug:
+            response.content = "Malformed email address"
+            response.status_code = 406
+        finally
             return response
 
 # Example: http://documentation.mailgun.com/quickstart-receiving.html#supported-actions-for-routes
