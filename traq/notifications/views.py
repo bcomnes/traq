f2aefd353b65a51719e7ccc7a901c628576b3ea2
Bret Comnes <bcomnes@gmail.com>
2014-09-17 16:59:35 -0700

    Users create todos, staff create tickets

--- traq/notifications/views.py
+++ traq/notifications/views.py
@@ -7,20 +7,22 @@ from django.core.validators import EmailValidator
 from django.contrib.auth.models import User
 from traq.projects.models import Project, Component
 from traq.tickets.models import Ticket, TicketStatus, TicketPriority
+from traq.todos.models import ToDo
 
 from traq.local_settings import MAILGUN_API_KEY
 
 validate_email = EmailValidator()
 
 class DoesNotVerify(Exception):
+    """Exception when the request isn't a valid mailgun POST"""
     def __str__(self):
         return "Request failed to validate"
 
 def verify(api_key, token, timestamp, signature):
+    """Verifies the request against the mailgun MAILGUN_API_KEY"""
     # See http://documentation.mailgun.com/user_manual.html#webhooks
-    req_hash = hmac.new(
-                        key=api_key,
-                        msg= timestamp + token,
+    req_hash = hmac.new(key=api_key,
+                        msg=timestamp + token,
                         digestmod=hashlib.sha256).hexdigest()
     if signature == req_hash:
         return
@@ -28,11 +30,13 @@ def verify(api_key, token, timestamp, signature):
         raise DoesNotVerify()
 
 def parse_email(address):
+    """Returns a dictionary with the user part and domain part of an email address after making sure the address is a valid email address"""
     validate_email(address)
     user_part, domain_part = address.rsplit('@', 1)
     return {'user_part':user_part, 'domain_part':domain_part}
 
 def create_ldap_set(address):
+    """Returns a set of possible alternate email addresses without the input email address"""
     qs = "(| (mail={0}) (mailRoutingAddress={0}) (mailLocalAddress={0}))".format(address)
     results = ldap.ldapsearch(qs)
     
@@ -43,12 +47,14 @@ def create_ldap_set(address):
     return set(mail + mailRoutingAddress + mailLocalAddress) - set([address])
 
 def create_new_user(address):
-    new_user = User.objects.created_byuser(sender, email=sender)
+    """Create and return a new User object based on their email address"""
+    new_user = User.objects.create_user(address, email=address)
     new_user.save()
     return new_user
 
 @csrf_exempt
 def index(request):
+    #import pdb; pdb.set_trace()
     if request.method == 'GET':
         return HttpResponse("Hi!  You don't look like email")
 
@@ -62,7 +68,7 @@ def index(request):
             recipient = request.POST['recipient']
             sender = request.POST['sender']
             subject = request.POST['subject']
-            body = request.POST['body-plain']
+            body = request.POST['stripped-text']
         except KeyError:
             response.content = "Missing critical fields"
             response.status_code = 406
@@ -85,32 +91,34 @@ def index(request):
             return response
 
         try:
-            project = Project.objects.get(slug = slug)
+            project = Project.objects.get(slug=slug)
         except Project.DoesNotExist:
             response.content = "That project does not exist"
             response.status_code = 406
             return response
 
         try:
-            created_by = User.objects.get(email = sender)
+            created_by = User.objects.get(email=sender)
         except User.DoesNotExist:
             if sender_domain != 'pdx.edu':
                 created_by = create_new_user(sender)
             else:
-                alt_users = User.objects.filter(email__in= list(create_ldap_set(sender)))
+                alt_users = User.objects.filter(email__in=list(create_ldap_set(sender)))
                 if len(alt_users) > 0:
                     created_by = alt_users[0]
                 else:
                     created_by = create_new_user(sender)
 
-        if created_by.groups.filter(name__in = ['arcstaff','arc']).exists():
-            status = TicketStatus.objects.get(is_default=True)
-            priority = TicketPriority.objects.get(is_default=True)
-            component = Component.objects.get(is_default=True, project = project)
-            
-            t = Ticket(project=project, title=subject, body = body, created_by = created_by, status = status, priority = priority, component = component)
-            t.save()
+        status = TicketStatus.objects.get(is_default=True)
+        priority = TicketPriority.objects.get(is_default=True)
+        component = Component.objects.get(is_default=True, project = project)
+
+        if created_by.groups.filter(name__in=['arcstaff', 'arc']).exists():
+            new_ticket = Ticket(project=project, title=subject, body = body, created_by = created_by, status = status, priority = priority, component = component)
+            new_ticket.save()
             response.content = "A new ticket has been created"
         else:
-            # Create todo
-        return response
+            new_todo = ToDo(project=project, title=subject, body = body, created_by = created_by, status = status, priority = priority, component = component)
+            new_todo.save()
+            response.content = "A new todo has been created"
+        return response 
